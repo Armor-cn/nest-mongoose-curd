56f52229d8da70e0767a045db10910f6c73c30d9
1669487366 <1669487366@qq.com>
2019-09-09 14:01:03 +0800

    fix：

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,42 +2,42 @@ import { Injectable, Logger, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './types/user';
-import { CreateUserDto } from '../user/dto/create-user.dto';
-import { LoginUserDto } from '../user/dto/login-user.dto';
-import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create.user.dto';
+import { LoginUserDto } from './dto/login.user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
-    constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+    constructor(@InjectModel('User') private readonly userModel: Model<User>) { }
 
     sanitizeUser(user: User) {
-        // const sanitized = user.toObject();
-        // delete sanitized.password;
-        // return sanitized;
+        user.password = ''
+        // delete user.password;
+        return user;
     }
 
-    async create(userDTO: CreateUserDto) {
-        const {email} = userDTO;
-        const user = await this.userModel.findOne({email});
+    async create(userDto: CreateUserDto) {
+        const { email } = userDto;
+        const user = await this.userModel.findOne({ email });
         if (user) {
-            throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+            throw new HttpException('用户名已经存在', HttpStatus.BAD_REQUEST);
         }
 
-        const createdUser = new this.userModel(userDTO);
+        const createdUser = new this.userModel(userDto);
         await createdUser.save();
-        // return this.sanitizeUser(createdUser);
+        return this.sanitizeUser(createdUser);
     }
 
     async findByLogin(userDTO: LoginUserDto) {
-        const {email, password} = userDTO;
-        const user = await this.userModel.findOne({email});
+        const { email, password } = userDTO;
+        const user = await this.userModel.findOne({ email });
         if (!user) {
             throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
         }
 
         if (await bcrypt.compare(password, user.password)) {
-            return ;
-            // return this.sanitizeUser(user);
+            return this.sanitizeUser(user);
+
         } else {
             throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
         }
